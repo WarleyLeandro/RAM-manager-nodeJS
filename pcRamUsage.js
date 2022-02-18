3994bcb8a0243215ca757a4cd6fbeae078875ae5
WarleyLeandro <warley.leandro95@gmail.com>
2022-02-17 21:57:29 -0300

    finish work

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,15 +1,24 @@
-const os = require('os')
+const os = require("os");
 
-const {arch, platform, totalmem, freemem} = os
+setInterval(() => {
+  const { arch, platform, totalmem, freemem } = os;
 
-const tRam = totalmem() / 1024 / 1024
-const fRam = freemem() /1024 / 1024
+  const tRam = totalmem() / 1024 / 1024;
+  const fRam = freemem() / 1024 / 1024;
+  const usage = (fRam / tRam) * 100;
 
-const stats = {
+  const stats = {
     OS: platform(),
     Arch: arch(),
-    TotalRAM: parseInt(tRam) ,
-    FreeRAM: parseInt(fRam)
-}
+    TotalRAM: parseInt(tRam),
+    FreeRAM: parseInt(fRam),
+    Usage: `${usage.toFixed(2)}%`,
+  };
 
-console.log(stats)
+  console.clear();
+  console.table(stats);
+
+  exports.stats = stats
+
+}, 1000);
+
--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const http = require('http')
 
 const host ='http://localhost'
 const port = 5000
+const stats = require('./pcRamUsage')
 
 http.createServer((req, res) => {
-    res.end('<h1> foiii</h1>')
-}).listen(5000, () => console.log(`rodando...${host}:${port}`))
+    let url = req.url
+
+    if(url === '/stats') {
+        res.end(JSON.stringify(stats, null, 2))
+    } else {
+        res.end('<h1> Seja bem vindo</h1>')
+    }
+}).listen(port, () => console.log(`rodando...${host}:${port}, ${stats}`))
